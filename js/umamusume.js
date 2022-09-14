885a59fac5878910a69cb58a30f6273fcb025c77
ironpad_ryujo <70073019+quantum818@users.noreply.github.com>
2022-09-15 02:14:05 +0900

    Site updated: 2022-09-15 02:14:04

---
+++ js/umamusume.js
@@ -0,0 +1,101 @@
+let html = '<style>.umamusume-newUma{background:#ff3852}.umamusume-twitter{background:#ff9406}.weibo-jyzy{background:#ffc000}.umamusume-supportCard{background:#00b7ee}.weibo-adrecommend{background:#febd22}.weibo-friend{background:#8fc21e}.weibo-boom{background:#bd0000}.umamusume-notice{background:#ff6f49}.umamusume-notice-ad{background:#332B33}.umamusume-activity{background:#f86400}#weibo-container{overflow-y:auto;-ms-overflow-style:none;scrollbar-width:none}#weibo-container::-webkit-scrollbar{display:none}.weibo-list-item{display:flex;flex-direction:row;justify-content:space-between;flex-wrap:nowrap}.weibo-title{white-space:nowrap;overflow:hidden;text-overflow:ellipsis;margin-right:auto}.weibo-num{float:right}.weibo-hotness{display:inline-block;padding:0 6px;transform:scale(.8) translateX(-3px);color:#fff;border-radius:8px}</style>'
+html += '<div class="weibo-list">'
+let hotness = {
+    '日服公告': 'umamusume-notice-ad',
+    '周报速览': 'umamusume-twitter',
+    '当前活动': 'umamusume-activity',
+    '马娘卡池': 'umamusume-newUma',
+    '支援卡池': 'umamusume-supportCard'
+}
+var url = "https://wiki.biligame.com/umamusume/api.php"; 
+var targetUrl = "https://wiki.biligame.com/umamusume/";
+var params1 = {
+    "action": "query",
+    "format": "json",
+    "list": "search",
+    "srsearch": "公告",
+    "srsort":"last_edit_desc",
+    "srwhat":"text",
+    "srprop":"size|wordcount|timestamp|snippet"
+};
+var params2 = {
+    "action": "query",
+    "format": "json",
+    "list": "search",
+    "srsearch": "赛马娘卡池",
+    "srsort":"last_edit_desc",
+    "srwhat":"text",
+    "srprop":"size|wordcount|timestamp|snippet"
+};
+var params3 = {
+    "action": "query",
+    "format": "json",
+    "list": "search",
+    "srsearch": "剧情活动",
+    "srsort":"last_edit_desc",
+    "srwhat":"text",
+    "srprop":"size|wordcount|timestamp|snippet"
+};
+var params4 = {
+    "action": "query",
+    "format": "json",
+    "list": "search",
+    "srsearch": "支援卡卡池",
+    "srsort":"last_edit_desc",
+    "srwhat":"text",
+    "srprop":"size|wordcount|timestamp|snippet"
+};
+var params5 = {
+    "action": "query",
+    "format": "json",
+    "list": "search",
+    "srsearch": "竞技场周报",
+    "srsort":"last_edit_desc",
+    "srwhat":"text",
+    "srprop":"size|wordcount|timestamp|snippet"
+};
+url = url + "?origin=*";
+//
+Object.keys(params1).forEach(function(key){url += "&" + key + "=" + params1[key];});
+fetch(url).then(response=>response.json()).then(response=>{
+    html += '<div class="weibo-list-item"><div class="weibo-hotness ' + hotness[('日服公告')] + '">' + ('日服公告') + '</div>' + '<span class="weibo-title"><a title="' + response.query.search[0].title + '"href="' + targetUrl + response.query.search[0].title + '" target="_blank" rel="external nofollow noreferrer">' + response.query.search[0].title + '</a></span>' + '</div>'
+    html += '</div>'
+    document.getElementById('weibo-container').innerHTML = html
+}).catch(function(error) {
+    console.log(error);
+});
+//
+Object.keys(params2).forEach(function(key){url += "&" + key + "=" + params2[key];});
+fetch(url).then(response=>response.json()).then(response=>{
+    html += '<div class="weibo-list-item"><div class="weibo-hotness ' + hotness[('马娘卡池')] + '">' + ('马娘卡池') + '</div>' + '<span class="weibo-title"><a title="' + response.query.search[0].title + '"href="' + targetUrl + response.query.search[0].title + '" target="_blank" rel="external nofollow noreferrer">' + response.query.search[0].title + '</a></span>' + '</div>'
+    html += '</div>'
+    document.getElementById('weibo-container').innerHTML = html
+}).catch(function(error) {
+    console.log(error);
+});
+//
+Object.keys(params4).forEach(function(key){url += "&" + key + "=" + params4[key];});
+fetch(url).then(response=>response.json()).then(response=>{
+    html += '<div class="weibo-list-item"><div class="weibo-hotness ' + hotness[('支援卡池')] + '">' + ('支援卡池') + '</div>' + '<span class="weibo-title"><a title="' + response.query.search[0].title + '"href="' + targetUrl + response.query.search[0].title + '" target="_blank" rel="external nofollow noreferrer">' + response.query.search[0].title + '</a></span>' + '</div>'
+    html += '</div>'
+    document.getElementById('weibo-container').innerHTML = html
+}).catch(function(error) {
+    console.log(error);
+});
+//
+Object.keys(params3).forEach(function(key){url += "&" + key + "=" + params3[key];});
+fetch(url).then(response=>response.json()).then(response=>{
+    html += '<div class="weibo-list-item"><div class="weibo-hotness ' + hotness[('当前活动')] + '">' + ('当前活动') + '</div>' + '<span class="weibo-title"><a title="' + response.query.search[0].title + '"href="' + targetUrl + response.query.search[0].title + '" target="_blank" rel="external nofollow noreferrer">' + response.query.search[0].title + '</a></span>' + '</div>'
+    html += '</div>'
+    document.getElementById('weibo-container').innerHTML = html
+}).catch(function(error) {
+    console.log(error);
+});
+Object.keys(params5).forEach(function(key){url += "&" + key + "=" + params5[key];});
+fetch(url).then(response=>response.json()).then(response=>{
+    html += '<div class="weibo-list-item"><div class="weibo-hotness ' + hotness[('周报速览')] + '">' + ('周报速览') + '</div>' + '<span class="weibo-title"><a title="' + response.query.search[1].title + '"href="' + targetUrl + response.query.search[1].title + '" target="_blank" rel="external nofollow noreferrer">' + response.query.search[1].title + '</a></span>' + '</div>'
+    html += '</div>'
+    document.getElementById('weibo-container').innerHTML = html
+}).catch(function(error) {
+    console.log(error);
+});
